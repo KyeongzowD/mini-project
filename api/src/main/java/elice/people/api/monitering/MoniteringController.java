f3401d404df6fe1c52359ed8258cb60dc9ba4fb7
김경은 <109902932+KyeongzowD@users.noreply.github.com>
2026-03-12 02:21:54 +0900

    init: Docker-compose(Springboot, Prometheus, Grafana) setup

---
+++ api/src/main/java/elice/people/api/monitering/MoniteringController.java
@@ -0,0 +1,14 @@
+package elice.people.api.monitering;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MoniteringController {
+
+//    @GetMapping
+//    public ResponseEntity<> error() {
+//
+//    }
+}
